f9bff15865e62702638f957c04bd2fc88180e136
DanMichaeli1 <33694802+DanMichaeli1@users.noreply.github.com>
2018-05-01 15:42:17 +0300

    manual mode WIP

--- manualMode.py
+++ manualMode.py
@@ -1,13 +1,29 @@
 __author__ = 'Dan'
 
+import time
+from datetime import datetime
 
-def compare_two_samples_by_time(processList, time1, time2):
+def fill_process_arr(processes_array, data, index):
+    for line in data[index:]:
+        if line == "-" * 80:
+            break
+        processes_array.append(line)
+    enumerate(processes_array)
+    return len(processes_array)
+
+
+
+def compare_two_samples_by_time(processList, time1, time2, time_variance = 0):
     processesTime1 = []
     processesTime2 = []
-    with open(processList, 'r') as myfile:
-        data = myfile.read().replace('\n', '')
-    for line in data:
-        if line == time1:
-            fill_process_arr(processesTime1, data)
+    line_index = 0
+    with open(processList + "\\processList.txt", 'r') as myfile:
+        data = myfile.read().splitlines()
+        for linenum, line in enumerate(data[line_index:]):
+            if line == time1:
+               line_index = fill_process_arr(processesTime1, data, linenum+2)
+            if line == time2:
+                fill_process_arr(processesTime2, data, linenum)
+
 
 
--- process_monitor.py
+++ process_monitor.py
@@ -1,7 +1,10 @@
 from processList import ProcessList
+from manualMode import compare_two_samples_by_time
 
 __author__ = 'Dan'
 
-interval = float(raw_input("Please input log update interval (in seconds): "))
-p = ProcessList(interval)
-p.write_log()
+#interval = float(raw_input("Please input log update interval (in seconds): "))
+p = ProcessList(5)
+#p.write_log()
+
+compare_two_samples_by_time(p.get_log(), "2018-05-01 13:28:45", "2018-05-01 13:28:51")
